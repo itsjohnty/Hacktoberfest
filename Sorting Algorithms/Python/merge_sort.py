07372a56053e2627b203cf8f41f02896c62ea1c4
Mayuresh Dharwadkar <98738585+Mayureshd-18@users.noreply.github.com>
2023-10-10 14:32:00 +0530

    Create merge_sort.py

---
+++ Sorting Algorithms/Python/merge_sort.py
@@ -0,0 +1,42 @@
+def merge_sort(arr):
+    """
+    Merge Sort Algorithm
+    
+    Parameters:
+    - arr: List to be sorted
+    """
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        # Merge the two halves
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        # Check for any remaining elements
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+# Example Usage:
+arr = [38, 27, 43, 3, 9, 82, 10]
+merge_sort(arr)
+print("Merge Sorted array:", arr)
